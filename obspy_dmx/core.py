f406b4c0378382eae84c3bdbf23fe7dbf7aa1e4b
ThomasLecocq <thlecocq@gmail.com>
2019-06-21 00:43:19 +0200

    rewrite to use numpy instead of struct

--- obspy_dmx/core.py
+++ obspy_dmx/core.py
@@ -3,48 +3,70 @@ from obspy import Stream, Trace, UTCDateTime
 import numpy as np
 import struct
 
+descript_trace_dtypes = np.dtype([('network', "4S"),
+                       ("st_name", "5S"),
+                       ("component", "1S"),
+                       ("insstype", np.int16),
+                       ("begintime", np.double),
+                       ("localtime", np.int16),
+                       ("datatype", "1S"),
+                       ("descriptor", "1S"),
+                       ("digi_by", np.int16),
+                       ("processed", np.int16),
+                       ("length", np.int32),
+                       ("rate", np.float32),
+                       ("mindata", np.float32),
+                       ("maxdata", np.float32),
+                       ("avenoise", np.float32),
+                       ("numclip", np.int32),
+                       ("timecorrect", np.double),
+                       ("rate_correct", np.float32),
+                       ])
+
+structtag_dtypes = np.dtype([("sinc", "1S"),
+                             ("machine", "1S"),
+                             ("id_struct", np.int16),
+                             ("len_struct", np.int32),
+                             ("len_data", np.int32)])
+
+types = {}
+types["s"] = ("uint16", "H", 2)
+types["q"] = ("int16", "h", 2)
+types["u"] = ("uint16", "H", 2)
+types["i"] = ("int16", "h", 2)
+types["2"] = ("int32", "i", 4)
+types["l"] = ("int32", "i", 4)
+types["r"] = ("uint16", "H", 2)
+types["f"] = ("float32", "f", 4)
+types["d"] = ("float64", "d", 8)
+
 def readstructtag(fid):
     y = AttribDict()
-    y.sinc, y.machine, y.id_struct, y.len_struct, y.len_data = struct.unpack("BBhii", fid.read(12))
+    data = np.fromfile(fid, structtag_dtypes, 1)
+    for (key, (format,size)) in structtag_dtypes.fields.items():
+        if str(format).count("S") != 0:
+            y[key] = data[key][0].decode('UTF-8')
+        else:
+            y[key] = data[key][0]
     return y
 
 
 def readdescripttrace(fid):
     y = AttribDict()
-    y.network = "".join(["%s" % chr(_) for _ in struct.unpack("BBBB", fid.read(4))]) # char(fread(fid, 4, 'uchar'))
-    y.st_name = "".join(["%s" % chr(_) for _ in struct.unpack("BBBBB", fid.read(5))])  # char(fread(fid, 5, 'uchar'))
-    y.component = chr(struct.unpack("B", fid.read(1))[0]) # char(fread(fid, 1, 'uchar'))
-    y.insstype = struct.unpack("h", fid.read(2))[0] # fread(fid, 1, 'int16')
-    y.begintime =  struct.unpack("d", fid.read(8))[0] # fread(fid, 1, 'double')
-    y.localtime =  struct.unpack("h", fid.read(2))[0] # fread(fid, 1, 'int16')
-    y.datatype = chr(struct.unpack("B", fid.read(1))[0]) # char(fread(fid, 1, 'uchar'))
-    y.descriptor = chr(struct.unpack("B", fid.read(1))[0])  # char(fread(fid, 1, 'uchar'))
-    y.digi_by = struct.unpack("h", fid.read(2))[0]  # fread(fid, 1, 'int16')
-    y.processed = struct.unpack("h", fid.read(2))[0]  # fread(fid, 1, 'int16')
 
-    y.length = struct.unpack("i", fid.read(4))[0]  # fread(fid, 1, 'int32')
-    y.rate =  struct.unpack("f", fid.read(4))[0]  # fread(fid, 1, 'float32')
-    y.mindata = struct.unpack("f", fid.read(4))[0]  # fread(fid, 1, 'float32')
-    y.maxdata = struct.unpack("f", fid.read(4))[0]  # fread(fid, 1, 'float32')
-    y.avenoise = struct.unpack("f", fid.read(4))[0]  # fread(fid, 1, 'float32')
-    y.numclip = struct.unpack("i", fid.read(4))[0]  # fread(fid, 1, 'int32')
-    y.timecorrect = struct.unpack("d", fid.read(8))[0] # fread(fid, 1, 'double')
-    y.rate_correct = struct.unpack("f", fid.read(4))[0]  # fread(fid, 1, 'float32')
+    data = np.fromfile(fid, descript_trace_dtypes, 1)
+
+    for (key, (format,size)) in descript_trace_dtypes.fields.items():
+        if str(format).count("S") != 0:
+            y[key] = data[key][0].decode('UTF-8')
+        else:
+            y[key] = data[key][0]
+
     return y
 
 def readdata(fid, n, t):
-    types = {}
-    types["s"] = ("uint16", "H", 2)
-    types["q"] = ("int16", "h", 2)
-    types["u"] = ("uint16", "H", 2)
-    types["i"] = ("int16", "h", 2)
-    types["2"] = ("int32", "i", 4)
-    types["l"] = ("int32", "i", 4)
-    types["r"] = ("uint16", "H", 2)
-    types["f"] = ("float32", "f", 4)
-    types["d"] = ("float64", "d", 8)
     target = types[t]
-    return struct.unpack(target[1] * n, fid.read(n*target[2]))
+    return np.fromfile(fid, eval("np.%s"%target[0]), n)
 
 
 def _is_dmx(filename):
@@ -78,20 +100,17 @@ def _read_dmx(filename, head_only=None, **kwargs):
             structtag = readstructtag(fid)
             if structtag.id_struct == 7:
                 descripttrace = readdescripttrace(fid)
-                if station is None or descripttrace.st_name.rstrip(
-                        '\x00').rstrip(' ') == station:
+                if station is None or descripttrace.st_name.strip() == station:
                     data = readdata(fid, descripttrace.length,
                                     descripttrace.datatype)
                     tr = Trace(data=np.asarray(data))
-                    tr.stats.network = descripttrace.network.rstrip(
-                        '\x00').rstrip(' ')
-                    tr.stats.station = descripttrace.st_name.rstrip(
-                        '\x00').rstrip(' ')
-                    tr.stats.channel = descripttrace.component.rstrip(
-                        '\x00').rstrip(' ')
+                    tr.stats.network = descripttrace.network.strip()
+                    tr.stats.station = descripttrace.st_name.strip()
+                    tr.stats.channel = descripttrace.component
                     tr.stats.sampling_rate = descripttrace.rate
                     tr.stats.starttime = UTCDateTime(descripttrace.begintime)
-                    tr.stats.dmx = descripttrace
+                    tr.stats.dmx = AttribDict({"descripttrace":descripttrace,
+                                               "structtag":structtag})
                     traces.append(tr)
                 else:
                     fid.seek(int(structtag.len_data), 1)
@@ -99,5 +118,6 @@ def _read_dmx(filename, head_only=None, **kwargs):
                 fid.seek(int(structtag.len_struct) + int(structtag.len_data), 1)
 
     st = Stream(traces=traces)
+    # print(st)
     return st
 
